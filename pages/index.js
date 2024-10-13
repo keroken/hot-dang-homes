2c3a8265a359e9f3a38e1531dfc13a3a78929466
keroken <kenichi.shinagawa@gmail.com>
2024-10-14 02:10:46 +0900

    add call to action button to index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,11 @@ export default function Home(props) {
   console.log("Props", props);
   return (
     <div>
-      <MainMenu items={props.mainMenuItems} />
+      <MainMenu
+        items={props.mainMenuItems}
+        callToActionLabel={props.callToActionLabel}
+        callToActionDestination={props.callToActionDestination}
+      />
       <BlockRenderer blocks={props.blocks} />
     </div>
   );
@@ -27,6 +31,14 @@ export const getStaticProps = async () => {
         }
         acfOptionsMainMenu {
           mainMenu {
+            callToActionButton {
+              label
+              destination {
+                ... on Page {
+                  uri
+                }
+              }
+            }
             menuItems {
               menuItem {
                 destination {
@@ -54,6 +66,8 @@ export const getStaticProps = async () => {
   return {
     props: {
       mainMenuItems: mapMainMenuItems(data.acfOptionsMainMenu.mainMenu.menuItems),
+      callToActionLabel: data.acfOptionsMainMenu.mainMenu.callToActionButton.label,
+      callToActionDestination: data.acfOptionsMainMenu.mainMenu.callToActionButton.destination.uri,
       blocks,
     }
   }
