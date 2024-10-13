24901877b9820d0b09d58730a2c922270e562683
keroken <kenichi.shinagawa@gmail.com>
2024-10-13 17:14:46 +0900

    create Heading component

---
+++ components/Heading/Heading.js
@@ -0,0 +1,11 @@
+import { getFontSizeForHeading, getTextAlign } from "utils/fonts";
+import React from "react";
+
+export const Heading  = ({ textAlign, content, level = 2 }) => {
+  const tag = React.createElement(`h${level}`, {
+    dangerouslySetInnerHTML: { __html: content },
+    className: `font-heading max-w-5xl mx-auto my-5 ${getFontSizeForHeading(level)} ${getTextAlign(textAlign)}`,
+  });
+
+  return tag;
+};
