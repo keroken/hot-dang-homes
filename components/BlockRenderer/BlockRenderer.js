bab0d190faf11963bef56a38d58f0fcc33fb404e
keroken <kenichi.shinagawa@gmail.com>
2024-10-13 23:21:50 +0900

    add Paragraph to BlockRender

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,14 +1,26 @@
 import { Cover } from "../Cover";
 import { Heading } from "../Heading";
+import { Paragraph } from "../Paragraph";
+import { theme } from "../../theme";
 
 export const BlockRenderer = ({ blocks }) => {
   return blocks.map(block => {
     switch(block.name) {
+      case "core/paragraph": {
+        return (
+          <Paragraph
+            key={block.id}
+            textAlign={block.attributes.textAlign}
+            content={block.attributes.content}
+            textColor={theme[block.attributes.textColor] || block.attributes.style?.color?.text}
+          />
+        );
+      }
       case "core/heading": {
         return (
           <Heading
             key={block.id}
-            textAlign={block.attributes.textAlign}
+            textAlign={block.attributes.align}
             content={block.attributes.content}
             level={block.attributes.level}
           />
