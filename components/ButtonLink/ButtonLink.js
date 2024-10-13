15ba8394953c109e009cab44e9048a32a4cfb780
keroken <kenichi.shinagawa@gmail.com>
2024-10-14 02:23:45 +0900

    create ButtonLink component

---
+++ components/ButtonLink/ButtonLink.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export const ButtonLink = ({label, destination}) => {
+  return (
+    <Link href={destination} className="btn">
+      {label}
+    </Link>
+  );
+};
